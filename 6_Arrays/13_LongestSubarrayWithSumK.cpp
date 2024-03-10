87b2f0564e33cd693d2e0bf1a684042b8cb6d66e
The-EleetCoder <jaijain1803@gmail.com>
2024-03-10 17:15:56 +0530

    day 11

---
+++ 6_Arrays/13_LongestSubarrayWithSumK.cpp
@@ -0,0 +1,23 @@
+int longestSubarrayWithSumK(vector<int> a, long long k) {
+    // Write your code here
+    int right = 0;
+    int left = 0;
+    int sum = a[0];
+    int length = 0;
+    int n = a.size();
+
+    while (right<n){
+        while (left<=right && sum>k){
+            sum-=a[left];
+            left++;
+        } 
+        if (sum==k){
+            length = max(length, right-left+1);
+        }
+        right++;
+        if (right < n){
+            sum+=a[right];
+        }
+    }
+    return length;
+}
