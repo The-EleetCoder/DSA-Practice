ea8a98348e22132d6398931a440a29137b31cb36
The-EleetCoder <jaijain1803@gmail.com>
2024-03-10 23:48:49 +0530

    day 11

--- 6_Arrays/15_TwoSum.cpp
+++ 6_Arrays/15_TwoSum.cpp
@@ -1,18 +1,41 @@
+// ----> O(n2)
+
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         vector <int> arr;
+//         for (int i=0;i<nums.size();i++){
+//             int sum =nums[i];
+//             for (int j=i+1;j<nums.size();j++){
+//                 if (sum+nums[j]==target){
+//                     arr.push_back(i);
+//                     arr.push_back(j);
+//                     break;
+//                 }
+//             }
+//             if (arr.size()>0) break;
+//         }
+//         return arr;
+//     }
+// };
+
+// ------> O(nlogn)
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector <int> arr;
+        map <int,int> mpp;
+        vector <int> ans;
         for (int i=0;i<nums.size();i++){
-            int sum =nums[i];
-            for (int j=i+1;j<nums.size();j++){
-                if (sum+nums[j]==target){
-                    arr.push_back(i);
-                    arr.push_back(j);
-                    break;
-                }
+            int element = nums[i];
+            int more = target - element;
+            if (mpp.find(more) != mpp.end()){
+                ans.push_back(i);
+                ans.push_back(mpp[more]);
+                break;
             }
-            if (arr.size()>0) break;
+            mpp[element] = i;
         }
-        return arr;
+        return ans;
     }
 };
