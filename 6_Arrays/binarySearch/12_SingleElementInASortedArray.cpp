a3faadf9d94e204bd99b662f0dba21325894623d
The-EleetCoder <jaijain1803@gmail.com>
2024-04-22 16:30:34 +0530

    day 23

---
+++ 6_Arrays/binarySearch/12_SingleElementInASortedArray.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int low = 0;
+        int high = nums.size() - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (mid % 2 == 1) {
+                mid--; // ensure mid is always on an even index
+            }
+
+            if (nums[mid] == nums[mid + 1]) {
+                low = mid + 2;
+            } else {
+                high = mid;
+            }
+        }
+        return nums[low];
+    }
+};
